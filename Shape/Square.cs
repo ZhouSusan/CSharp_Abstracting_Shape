e0219045eb147186cd6511b57a523f23a62a4d61
ZhouSusan <szhou089@gmail.com>
2022-12-11 15:38:04 -0800

    Added Square subclass with abstract methods in Square.cs

---
+++ Shape/Square.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSharp_Abstracting_Shape.Shape
+{
+    internal class Square : GeometricShape
+    {
+        private double _side;
+        public Square(double side)
+        {
+            _side = side;
+        }
+        public override double area()
+        {
+            return (_side * _side);
+        }
+
+        public override double perimeter()
+        {
+            return (4*_side);
+        }
+    }
+}

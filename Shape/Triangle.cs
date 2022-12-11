62e9a76bde89767a935ad80c1c5e263bbbcddf02
ZhouSusan <szhou089@gmail.com>
2022-12-11 15:29:29 -0800

    Added Triangle subclass with abstract methods

---
+++ Shape/Triangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CSharp_Abstracting_Shape.Shape
+{
+    public class Triangle : GeometricShape
+    {
+        private double _num1, _num2, _num3;
+
+        public Triangle(double num1, double num2)
+        {
+            _num1 = num1;
+            _num2 = num2;   
+        }
+
+        public Triangle(double num1, double num2, double num3)
+        {
+            _num1 = num1;
+            _num2 = num3;
+            _num3 = num3;
+        }
+        public override double area()
+        {
+            return 0.5*(_num1*_num2);
+        }
+
+        public override double perimeter()
+        {
+            return _num1 + _num2 + _num3;
+        }
+    }
+}

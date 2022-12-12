703b5ea1d10b971c45be9e5ebbc592f53011981b
Javier Tamez <tamezjavier@gmail.com>
2022-12-11 18:38:46 -0600

    Implement tests for all classes in Main

--- Program.cs
+++ Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Abstrction in oop");
+﻿using CSharp_Abstracting_Shape.Shape;
+using CSharp_Abstracting_Shape;
+
+namespace CSharp_Abstracting_Shape
+{
+        public class Program
+    {
+        public static void Main()
+        {
+            testCircle();
+            testSquare();
+            testTriangle();
+            testOval();
+        }
+        private static void testCircle()
+        {
+            Console.WriteLine("Should not throw an exception.");
+            Circle circle1 = new Circle(5);
+            Console.WriteLine("Area of this circle should be: 78.5.");
+            Console.WriteLine("We got: " + circle1.area());
+            Console.WriteLine("The perimeter of our circle should be: 31.4");
+            Console.WriteLine("We got: " + circle1.perimeter());
+        }
+        private static void testSquare() 
+        {
+            Console.WriteLine("Should not throw an exception.");
+            Square square1 = new Square(5);
+            Console.WriteLine("Area of this square should be: 25.0.");
+            Console.WriteLine("We got: " + square1.area());
+            Console.WriteLine("The perimeter of our square should be: 20.0");
+            Console.WriteLine("We got: " + square1.perimeter());
+        }
+        private static void testTriangle()
+        {
+            Console.WriteLine("Should not throw an exception.");
+            Triangle triangle1 = new Triangle(3, 4, 5);
+            Console.WriteLine("Area of this triangle should be: 6.0.");
+            Console.WriteLine("We got: " + triangle1.area());
+            Console.WriteLine("The perimeter of our triangle should be: 12.0");
+            Console.WriteLine("We got: " + triangle1.perimeter());
+        }
+        private static void testOval()
+        {
+            Console.WriteLine("Should not throw an exception.");
+            Oval oval1 = new Oval(1, 2);
+            Console.WriteLine("Area of this oval should be: 6.28.");
+            Console.WriteLine("We got: " + oval1.area());
+            Console.WriteLine("The perimeter of our oval should be: 9.93");
+            Console.WriteLine("We got: " + oval1.perimeter());
+        }
+    }
+}
